6fcc6196874824e355452c24e8961f4e9d183287
Vedant <vedantnp200@gmail.com>
2023-06-18 22:44:45 +0530

    [Vedant]-Adding a new contact in address book

---
+++ AddressBook.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Address_Book_System
+{
+    internal class AddressBook
+    {
+
+        Contact contact = new Contact();
+
+        public void AddContact()
+        {
+            Console.Write("Enter firstname: ");
+            contact.FirstName = Console.ReadLine();
+            Console.Write("Enter lastname: ");
+            contact.LastName = Console.ReadLine();
+            Console.Write("Enter email: ");
+            contact.Email = Console.ReadLine();
+            Console.Write("Enter phone number: ");
+            contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Enter address: ");
+            contact.Address = Console.ReadLine();
+            Console.Write("Enter city: ");
+            contact.City = Console.ReadLine();
+            Console.Write("Enter state: ");
+            contact.State = Console.ReadLine();
+            Console.Write("Enter postalcode: ");
+            contact.ZipCode = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void DisplayContact()
+        {
+            Console.WriteLine("Name: {0} {1}", contact.FirstName, contact.LastName);
+            Console.WriteLine("Email: " + contact.Email);
+            Console.WriteLine("Phone Number: " + contact.PhoneNumber);
+            Console.WriteLine("Address: " + contact.Address);
+            Console.WriteLine("City: " + contact.City);
+            Console.WriteLine("State: " + contact.State);
+            Console.WriteLine("Postal Code: " + contact.ZipCode);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,35 @@ namespace Address_Book_System
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("----Welcome to Address Book System----");
             Console.ResetColor();
+
+            AddressBook book = new AddressBook();
+
+            while (true)
+            {
+                Console.WriteLine("\nSelect an option: ");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Display Contact");
+                Console.WriteLine("0. Exit");
+                Console.Write("=> ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        book.AddContact();
+                        break;
+                    case 2:
+                        book.DisplayContact();
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Enter valid option.");
+                        break;
+                }
+            }
+
         }
     }
 }
