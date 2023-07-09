1e79f5ef4a48a471d8ff65790f2546be45f92b6e
Vedant <vedantnp200@gmail.com>
2023-07-09 14:22:11 +0530

    [Vedant]-Manage address book service on SQL Server

--- Program.cs
+++ Program.cs
@@ -12,87 +12,111 @@ namespace Address_Book_System
             Console.WriteLine("----Welcome to Address Book System----");
             Console.ResetColor();
 
-            AddressBookController controller = new AddressBookController();
+            int option;
 
-            controller.ExistingAddressBook();
-            controller.ExistingContacts();
+            Console.WriteLine("\nSelect an option: ");
+            Console.WriteLine("1. Manage Address Book and Contact Details in List Collection.");
+            Console.WriteLine("2. Manage Address Book and Contact Details in MS SQL Server");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("==> ");
+            option = Convert.ToInt32(Console.ReadLine());
 
-            while (true)
+            switch (option)
             {
-                Console.WriteLine("\nSelect an option: ");
-                Console.WriteLine("1. Add New Address Book");
-                Console.WriteLine("2. Add New Contact in Existing Address Book");
-                Console.WriteLine("3. Display all contacts");
-                Console.WriteLine("4. Display all address books");
-                Console.WriteLine("5. Modify an existing Contact");
-                Console.WriteLine("6. Delete an existing data");
-                Console.WriteLine("7. Find contact details by City or State");
-                Console.WriteLine("8. View contact details by City or State");
-                Console.WriteLine("9. Count contact details by City or State");
-                Console.WriteLine("10. Sort contacts by Name in Ascending order");
-                Console.WriteLine("11. Sort contacts by City or State or Zip Code");
-                Console.WriteLine("12.Write and Read Address Book and Contacts details to TEXT file");
-                Console.WriteLine("13. Write and Read Address Book and Contacts details to CSV file");
-                Console.WriteLine("14. Write and Read Address Book and Contacts details to JSON file");
-                Console.WriteLine("0. Exit");
-                Console.Write("=> ");
-                int option = Convert.ToInt32(Console.ReadLine());
+                case 1:
+                    AddressBookController controller = new AddressBookController();
 
-                switch (option)
-                {
-                    case 1:
-                        controller.AddNewAddressBook();
-                        break;
-                    case 2:
-                        controller.AddContact();
-                        break;
-                    case 3:
-                        controller.DisplayContact();
-                        break;
-                    case 4:
-                        controller.DisplayAddressBook();
-                        break;
-                    case 5:
-                        controller.ModifyContactInAddressBook();
-                        break;
-                    case 6:
-                        controller.DeleteContactFromAddressBook();
-                        break;
-                    case 7:
-                        controller.FindContactByCityOrStateInAddressBook();
-                        break;
-                    case 8:
-                        controller.ViewContactByCityOrStateInAddressBook();
-                        break;
-                    case 9:
-                        controller.CountContactByCityOrState();
-                        break;
-                    case 10:
-                        controller.SortContactsByNameInAscOrder();
-                        break;
-                    case 11:
-                        controller.SortContactByCityOrStateOrZipCode();
-                        break;
-                    case 12:
-                        controller.WriteToFileUsingFileIO();
-                        controller.ReadFromFileUsingFileIO();
-                        break;
-                    case 13:
-                        controller.WriteToCSVFileUsingCSVHelper();
-                        controller.ReadFromCSVFileUsingCSVHelper();
-                        break;
-                    case 14:
-                        controller.WriteToJSONFileUsingNewtonsoftJson();
-                        controller.ReadFromJSONFileUsingNewtonsoftJson();
-                        break;
-                    case 0:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Enter valid option.");
-                        break;
-                }
+                    controller.ExistingAddressBook();
+                    controller.ExistingContacts();
+
+                    while (true)
+                    {
+                        Console.WriteLine("\nSelect an option: ");
+                        Console.WriteLine("1. Add New Address Book");
+                        Console.WriteLine("2. Add New Contact in Existing Address Book");
+                        Console.WriteLine("3. Display all contacts");
+                        Console.WriteLine("4. Display all address books");
+                        Console.WriteLine("5. Modify an existing Contact");
+                        Console.WriteLine("6. Delete an existing data");
+                        Console.WriteLine("7. Find contact details by City or State");
+                        Console.WriteLine("8. View contact details by City or State");
+                        Console.WriteLine("9. Count contact details by City or State");
+                        Console.WriteLine("10. Sort contacts by Name in Ascending order");
+                        Console.WriteLine("11. Sort contacts by City or State or Zip Code");
+                        Console.WriteLine("12.Write and Read Address Book and Contacts details to TEXT file");
+                        Console.WriteLine("13. Write and Read Address Book and Contacts details to CSV file");
+                        Console.WriteLine("14. Write and Read Address Book and Contacts details to JSON file");
+                        Console.WriteLine("0. Exit");
+                        Console.Write("=> ");
+                        option = Convert.ToInt32(Console.ReadLine());
+
+                        switch (option)
+                        {
+                            case 1:
+                                controller.AddNewAddressBook();
+                                break;
+                            case 2:
+                                controller.AddContact();
+                                break;
+                            case 3:
+                                controller.DisplayContact();
+                                break;
+                            case 4:
+                                controller.DisplayAddressBook();
+                                break;
+                            case 5:
+                                controller.ModifyContactInAddressBook();
+                                break;
+                            case 6:
+                                controller.DeleteContactFromAddressBook();
+                                break;
+                            case 7:
+                                controller.FindContactByCityOrStateInAddressBook();
+                                break;
+                            case 8:
+                                controller.ViewContactByCityOrStateInAddressBook();
+                                break;
+                            case 9:
+                                controller.CountContactByCityOrState();
+                                break;
+                            case 10:
+                                controller.SortContactsByNameInAscOrder();
+                                break;
+                            case 11:
+                                controller.SortContactByCityOrStateOrZipCode();
+                                break;
+                            case 12:
+                                controller.WriteToFileUsingFileIO();
+                                controller.ReadFromFileUsingFileIO();
+                                break;
+                            case 13:
+                                controller.WriteToCSVFileUsingCSVHelper();
+                                controller.ReadFromCSVFileUsingCSVHelper();
+                                break;
+                            case 14:
+                                controller.WriteToJSONFileUsingNewtonsoftJson();
+                                controller.ReadFromJSONFileUsingNewtonsoftJson();
+                                break;
+                            case 0:
+                                Environment.Exit(0);
+                                break;
+                            default:
+                                Console.WriteLine("Enter valid option.");
+                                break;
+                        }
+                    }
+                    break;
+                case 2:
+
+                    break;
+                case 0: 
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Enter valid option.");
+                    break;
             }
+
         }
     }
 }
